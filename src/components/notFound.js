f866ce5532f4d6f35f064e95e9007094ceedf71d
AdamBurysek <adam.burysek@senzam.cz>
2023-05-31 11:48:42 +0200

    Not found graphics add

--- src/components/notFound.js
+++ src/components/notFound.js
@@ -3,7 +3,12 @@ import React from "react";
 const NotFound = () => {
   return (
     <div>
-      <p>NOT FOUND</p>
+      <div
+        className="alert alert-danger text-center mt-5 container-sm"
+        role="alert"
+      >
+        Not Found
+      </div>
     </div>
   );
 };
